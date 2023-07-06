ca0e24751676d5993b9039e6842796b778417061
Feihong Hsu <feihong.hsu@gmail.com>
2023-07-06 18:07:08 -0500

    Only add cards that don't already exist

--- generate-cards.py
+++ generate-cards.py
@@ -4,10 +4,27 @@ import db
 words_file = Path('words.txt')
 word_cards_file = Path('cards.txt')
 
+def get_words_in_cards():
+  with word_cards_file.open('r', encoding='utf8') as fp:
+    for line in fp:
+      line = line.strip()
+      if line == '' or line.startswith('#'):
+        continue
+
+      # Assume the word is the second item
+      _note_type, word, *_rest = line.split(';')
+      yield word
+
 def get_words():
+  words_set = set(get_words_in_cards())
+
   with words_file.open('r', encoding='utf8') as fp:
     for line in fp:
-      yield line.strip()
+      line = line.strip()
+      if line == '' or line.startswith('#') or line in words_set:
+        continue
+
+      yield line
 
 def get_word_card_lines():
   for entry in db.get_entries_for_words(get_words()):
