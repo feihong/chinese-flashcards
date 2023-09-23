a25b69f586652980b3557cee8623becd1117142e
Feihong Hsu <feihong.hsu@gmail.com>
2023-09-23 15:59:16 -0500

    Delete unneeded content

--- publish.py
+++ publish.py
@@ -1,25 +1,15 @@
 import subprocess
-import shutil
 from pathlib import Path
-import urllib.parse
 
 content_dir = Path('content')
-
-output_dir = Path('public')
-if not output_dir.exists():
-  output_dir.mkdir()
-
-files = ['bookmarklet.html', 'asciimath-playground.html', 'speak-playground.html', 'listen-playground.html',
-         'asciimath2latex.js', 'asciimath-preview.js', 'index.html']
-for file in files:
-  shutil.copy(content_dir / file, output_dir)
 
 cmd = [
   'ghp-import',
   '--no-jekyll',
   '--push',
   '--no-history',
-  output_dir]
+  content_dir
+]
 subprocess.run(cmd)
 
 print(f'Deployed to https://feihong.github.io/chinese-flashcards/')
