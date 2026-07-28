a1d59540f904221de80bfdca1392f6a2674077b6
Feihong Hsu <feihong.hsu@gmail.com>
2026-07-27 19:07:58 -0500

    Add a helper function for showing review times and intervals for a card

--- anki_connect_explorer.py
+++ anki_connect_explorer.py
@@ -20,5 +20,14 @@ def getReviewsOfCards(*card_ids):
     return invoke("getReviewsOfCards", cards=card_ids)["result"]
 
 
+def show_review_times_for_cards(*card_ids):
+    res = getReviewsOfCards(*card_ids)
+    for card_id, reviews in res.items():
+        print(f"{card_id}:")
+        for review in reviews:
+            dt = datetime.fromtimestamp(review["id"] / 1000)
+            print(f"  {dt.isoformat()}, last interval: {review['lastIvl']}, interval: {review['ivl']}")
+
+
 if __name__ == "__main__":
-    IPython.embed(colors='Neutral')
+    IPython.embed(colors="Neutral")
